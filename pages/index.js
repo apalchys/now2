dfd73ab83e2e605d54192ac90778af254baf883c
Andrei Palchys <andrei_palchys@epam.com>
2019-04-24 19:55:09 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,67 @@
-export default () => (
-  <>
-    <div className="hero">
-    <h1 className="title">Offline Next.js with Now 2.0</h1>
-    </div>
+import React from "react";
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-        text-align: center;
-      }
-      .title {
-        font-family: sans-serif;
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-    `}</style>
-  </>
-)
+function getBrowser() {
+  if (window && window.navigator.platform.substr(0, 2) === "iP") {
+    //iOS (iPhone, iPod or iPad)
+    var lte9 = /constructor/i.test(window.HTMLElement);
+    var nav = window.navigator,
+      ua = nav.userAgent,
+      idb = !!window.indexedDB;
+    if (ua.indexOf("Safari") !== -1 && ua.indexOf("Version") !== -1 && !nav.standalone) {
+      return "Safari";
+    } else if ((!idb && lte9) || !window.statusbar.visible) {
+      return "UIWebView";
+    } else if ((window.webkit && window.webkit.messageHandlers) || !lte9 || idb) {
+      return "WKWebView";
+    }
+  }
+  return "n/a";
+}
+
+export default class Index extends React.Component {
+
+  constructor() {
+    this.state = {
+      browser: 'na'
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      browser: getBrowser()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="hero">
+          <h1 className="title">{this.state.browser}</h1>
+        </div>
+
+        <style jsx>{`
+          .hero {
+            width: 100%;
+            color: #333;
+            text-align: center;
+          }
+          .title {
+            font-family: sans-serif;
+            margin: 0;
+            width: 100%;
+            padding-top: 80px;
+            line-height: 1.15;
+            font-size: 48px;
+          }
+          .row {
+            max-width: 880px;
+            margin: 80px auto 40px;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
